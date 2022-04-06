3c36e4b37c1454bc0ca7ebb20de302c3cd1daab3
davidotvos <davidotvos@wtf.com>
2022-04-06 11:08:44 +0200

    added RegisterScene.fxml

--- src/main/java/hu/unideb/inf/controller/MainSceneController.java
+++ src/main/java/hu/unideb/inf/controller/MainSceneController.java
@@ -1,4 +1,25 @@
 package hu.unideb.inf.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MainSceneController {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    void handleLoginButtonPushed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleRegisterButtonPushed(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/hu/unideb/inf/controller/RegisterSceneController.java
+++ src/main/java/hu/unideb/inf/controller/RegisterSceneController.java
@@ -1,4 +1,36 @@
 package hu.unideb.inf.controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
 
 public class RegisterSceneController {
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private PasswordField pwConfirmLabel;
+
+    @FXML
+    private PasswordField pwLabel;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private CheckBox tcLabel;
+
+    @FXML
+    private PasswordField userLabel;
+
+    @FXML
+    void register(ActionEvent event) {
+
+    }
+
+    @FXML
+     void toLogin(ActionEvent event) {
+
+    }
 }
