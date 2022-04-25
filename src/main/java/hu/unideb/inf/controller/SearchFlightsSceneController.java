ef159bcafcc5ff3498388597dd68ed568cf88d1c
Vig Rudolf <vrudi@gmail.com>
2022-04-25 19:59:15 +0200

    SearchSceneController skeleton

--- src/main/java/hu/unideb/inf/controller/SearchFlightsSceneController.java
+++ src/main/java/hu/unideb/inf/controller/SearchFlightsSceneController.java
@@ -1,4 +1,51 @@
 package hu.unideb.inf.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class SearchFlightsSceneController {
+
+    @FXML
+    private CheckBox DestinationCheckbox;
+
+    @FXML
+    private ImageView FromCityPictures;
+
+    @FXML
+    private Button ReserveButton;
+
+    @FXML
+    private Label RightbarFromCity;
+
+    @FXML
+    private Label RightbarStartTimeLabel;
+
+    @FXML
+    private Label RightbarToCityLabel;
+
+    @FXML
+    private Button SearchButton;
+
+    @FXML
+    private TextField SearchTextfield;
+
+    @FXML
+    private CheckBox StartCheckbox;
+
+    @FXML
+    private ImageView ToCityPicture;
+
+    @FXML
+    void HandleReserveButton(ActionEvent event) {
+
+    }
+    @FXML
+    void HandleSearchButton(ActionEvent event) {
+
+    }
 }
